b1e5888f334e7453b9a4de8f76c101520bd2f5ef
Rahmat Nazali Salimi <rahmatnazali95@gmail.com>
2024-03-01 15:43:02 +0700

    Add default database setting for dev and test

--- source/starter/settings_test.py
+++ source/starter/settings_test.py
@@ -1,8 +1,14 @@
+import os
+
 from starter.settings import *
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'HOST': os.environ.get('POSTGRES_HOST', 'localhost'),
+        'PORT': os.environ.get('POSTGRES_PORT', '5432'),
+        'NAME': os.environ.get('POSTGRES_DB', 'starter'),
+        'USER': os.environ.get('POSTGRES_USER', 'user'),
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD', 'password'),
     }
 }
