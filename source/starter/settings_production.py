87f65d3efbc924eb35a23eaa2127ba58bebfd7e7
Rahmat Nazali Salimi <rahmatnazali95@gmail.com>
2024-03-04 17:32:13 +0700

    Add Whitenoise

--- source/starter/settings_production.py
+++ source/starter/settings_production.py
@@ -5,3 +5,16 @@ DEBUG = os.environ.get('DEBUG', False)
 ALLOWED_HOSTS = [
     '10.10.10.10'
 ]
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+STATIC_ROOT = BASE_DIR / "staticfiles"
