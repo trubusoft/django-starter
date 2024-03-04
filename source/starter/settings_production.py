a8150f14bf8992e008ed71ddab231f61da5aafba
Rahmat Nazali Salimi <rahmatnazali95@gmail.com>
2024-03-04 17:47:48 +0700

    Production Configuration (#7)
    * Add adapative database configuration
    
    * Add working gunicorn & postgres compose instance
    
    * Add production management script
    
    * Move production script inside source folder
    
    * Add gunicorn to production requirements
    
    * Remove unused import on settings_test.py
    
    * Add DJANGO_SETTINGS_MODULE to docker-compose.droplet.yaml
    
    * Add Dockerfile
    
    * Add adaptive DEBUG and ALLOWED_HOSTS
    
    * Bump gunicorn version
    
    * Remove unused database settings on settings_test.py
    
    * Update PyCharm testing configuration
    
    * Add Whitenoise
    
    * Add Whitenoise compression & caching
    
    * Update production volume name
    
    * Add missing import on settings_test.py

--- source/starter/settings_production.py
+++ source/starter/settings_production.py
@@ -1,1 +1,26 @@
 from starter.settings import *
+
+DEBUG = os.environ.get('DEBUG', False)
+
+ALLOWED_HOSTS = [
+    '10.10.10.10'
+]
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+STORAGES = {
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
+
+STATIC_ROOT = BASE_DIR / "staticfiles"
--- source/starter/settings_test.py
+++ source/starter/settings_test.py
@@ -1,14 +1,1 @@
-import os
-
 from starter.settings import *
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'HOST': os.environ.get('POSTGRES_HOST', 'localhost'),
-        'PORT': os.environ.get('POSTGRES_PORT', '5432'),
-        'NAME': os.environ.get('POSTGRES_DB', 'starter'),
-        'USER': os.environ.get('POSTGRES_USER', 'user'),
-        'PASSWORD': os.environ.get('POSTGRES_PASSWORD', 'password'),
-    }
-}
